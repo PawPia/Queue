01f6c651994fff23a5b86c0e8b78da78db413598
Pawel.Piasecki <250988@edu.p.lodz.pl>
2024-05-22 23:02:46 +0200

    added queue

---
+++ Queue.h
@@ -0,0 +1,12 @@
+//
+// Created by pawel on 22.05.2024.
+//
+
+#ifndef QUEUE_QUEUE_H
+#define QUEUE_QUEUE_H
+int isQueueEmpty(void);
+int isQueueFull(void);
+int remove(void);
+int first(void);
+void add(int val);
+#endif //QUEUE_QUEUE_H
